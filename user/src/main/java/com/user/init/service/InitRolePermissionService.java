72e3b6fe8ea735ac757ddb27af1ad738641de15e
y1nuo <xugaowei666@gmail.com>
2023-01-07 17:54:51 +0800

    add:userModel

--- user/src/main/java/com/user/init/service/InitRolePermissionService.java
+++ user/src/main/java/com/user/init/service/InitRolePermissionService.java
@@ -8,11 +8,11 @@ import com.user.po.RolePo;
 import com.user.service.ISysPermissionService;
 import com.user.service.ISysRolePermissionService;
 import com.user.service.ISysRoleService;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,6 +31,8 @@ public class InitRolePermissionService implements InitDataService {
     public void initData() {
         List<RolePo> rolePos = roleService.allRoleList();
 
+        Map<Long, String> collect1 = rolePos.stream().collect(Collectors.toMap(RolePo::getId, RolePo::getRoleCode));
+
         List<Long> roleIdList = rolePos.stream().map(RolePo::getId).collect(Collectors.toList());
 
         List<RolePermissionPo> rolePermissionList= rolePermissionService.getRolePermissionListByRoleIdList(roleIdList);
@@ -39,7 +41,15 @@ public class InitRolePermissionService implements InitDataService {
 
         List<PermissionPo> permission = permissionService.getPermissionByPermissionId(permissionIdList);
 
+        Map<Long, String> collect = permission.stream().collect(Collectors.toMap(PermissionPo::getId, PermissionPo::getUrl));
 
+        Map<Long, Long> roleIdPerIdMap = rolePermissionList.stream().collect(Collectors.toMap(RolePermissionPo::getRoleId, RolePermissionPo::getPermissionId));
+
+        for (Long roleId : roleIdList) {
+            Long aLong = roleIdPerIdMap.get(roleId);
+            String per = collect.get(aLong);
+            String role = collect1.get(aLong);
+        }
     }
 
     @Override
